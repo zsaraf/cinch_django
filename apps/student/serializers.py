38feda3dd65fd715164b9d8569c3efbe79b134a7
Raymond Kennedy <franzwarning@gmail.com>
2015-12-10 18:14:54 -0500

    Use coursegroupserializer instead of coursegroupmemberserialzer

--- apps/student/serializers.py
+++ apps/student/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import *
 from apps.tutoring.serializers import OpenSeshStudentSerializer, PastSeshStudentSerializer, OpenSeshRequestStudentSerializer
+from apps.group.serializers import CourseGroupSerializer
+from apps.group.models import CourseGroup, CourseGroupMember
 
 
 class FavoriteSerializer(serializers.ModelSerializer):
@@ -26,16 +28,19 @@ class StudentUserInfoSerializer(serializers.ModelSerializer):
 
 
 class StudentSerializer(serializers.ModelSerializer):
-    from apps.group.serializers import CourseGroupMemberSerializer
     favorite_set = FavoriteSerializer(many=True)
     open_seshes = OpenSeshStudentSerializer(many=True, source='opensesh_set')
     past_seshes = PastSeshStudentSerializer(many=True, source='pastsesh_set')
     open_requests = serializers.SerializerMethodField()
-    course_groups = CourseGroupMemberSerializer(many=True, source='coursegroupmember_set')
+    course_groups = serializers.SerializerMethodField()
     stats = serializers.ReadOnlyField()
 
     class Meta:
         model = Student
+
+    def get_course_groups(self, obj):
+        course_group_memberships = CourseGroupMember.objects.filter(student=obj)
+        return CourseGroupSerializer(CourseGroup.objects.filter(id__in=course_group_memberships.values('course_group_id')), many=True).data
 
     def get_open_requests(self, obj):
         from apps.tutoring.models import SeshRequest
