2a0546544a0282d6f9c2da393a5763aaa1fc59f9
Lilli Oetting <loetting@stanford.edu>
2016-02-08 22:46:32 -0800

    delay 1 second in between welcome messages

--- apps/account/views.py
+++ apps/account/views.py
@@ -15,6 +15,8 @@ from .serializers import *
 from apps.tutor.models import Tutor, PendingTutor
 from apps.student.models import Student
 import logging
+import time
+
 logger = logging.getLogger(__name__)
 
 
@@ -61,83 +63,83 @@ class TokenViewSet(viewsets.ModelViewSet):
 class ContestShareViewSet(viewsets.ModelViewSet):
     queryset = ContestShare.objects.all()
 
-    @list_route(methods=['get'])
-    def leaderboard(self, request):
-        from apps.chatroom.models import File, Upload
-        referral_leaders = ContestShare.objects.values('contest_code_id', 'contest_code__identifier').annotate(count=Count('contest_code_id')).order_by('-count')
-        uploads = File.objects.values('upload_id').annotate(count=Count('upload_id')).order_by('-count')
-        upload_leaders = {}
-        for u in uploads:
-            try:
-                upload = Upload.objects.get(pk=u['upload_id'])
-                share = ContestShare.objects.get(user=upload.chatroom_member.user)
-                if share.contest_code.identifier in upload_leaders:
-                    upload_leaders[share.contest_code.identifier] += int(u['count'])
-                else:
-                    upload_leaders[share.contest_code.identifier] = int(u['count'])
-            except ContestShare.DoesNotExist:
-                pass
+    # @list_route(methods=['get'])
+    # def leaderboard(self, request):
+    #     from apps.chatroom.models import File, Upload
+    #     referral_leaders = ContestShare.objects.values('contest_code_id', 'contest_code__identifier').annotate(count=Count('contest_code_id')).order_by('-count')
+    #     uploads = File.objects.values('upload_id').annotate(count=Count('upload_id')).order_by('-count')
+    #     upload_leaders = {}
+    #     for u in uploads:
+    #         try:
+    #             upload = Upload.objects.get(pk=u['upload_id'])
+    #             share = ContestShare.objects.get(user=upload.chatroom_member.user)
+    #             if share.contest_code.identifier in upload_leaders:
+    #                 upload_leaders[share.contest_code.identifier] += int(u['count'])
+    #             else:
+    #                 upload_leaders[share.contest_code.identifier] = int(u['count'])
+    #         except ContestShare.DoesNotExist:
+    #             pass
 
-        today = datetime.now().date()
-        num_days = today.weekday()
-        min_date = today - timedelta(days=num_days)
+    #     today = datetime.now().date()
+    #     num_days = today.weekday()
+    #     min_date = today - timedelta(days=num_days)
 
-        individual_referrals = SharePromo.objects.filter(is_past=True, timestamp__gt=min_date).values('old_user', 'old_user__email').annotate(count=Count('old_user')).order_by('-count')
+    #     individual_referrals = SharePromo.objects.filter(is_past=True, timestamp__gt=min_date).values('old_user', 'old_user__email').annotate(count=Count('old_user')).order_by('-count')
 
-        individual_uploads = File.objects.filter(timestamp__gt=min_date).values('upload__chatroom_member__user__email').annotate(count=Count('upload__chatroom_member__user__email')).order_by('-count')
+    #     individual_uploads = File.objects.filter(timestamp__gt=min_date).values('upload__chatroom_member__user__email').annotate(count=Count('upload__chatroom_member__user__email')).order_by('-count')
 
-        return render(request, 'test.html', {'referral_leaders': referral_leaders, 'upload_leaders': upload_leaders, 'individual_uploads': individual_uploads, 'individual_referrals': individual_referrals})
+    #     return render(request, 'test.html', {'referral_leaders': referral_leaders, 'upload_leaders': upload_leaders, 'individual_uploads': individual_uploads, 'individual_referrals': individual_referrals})
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserRegularInfoSerializer
 
-    @list_route(methods=['get', 'post'])
-    def team_dashboard(self, request):
-        from forms import AutoMessageForm
-        from apps.chatroom.models import ChatroomActivity, ChatroomActivityType, ChatroomMember, Message, ChatroomActivityTypeManager
-        from apps.chatroom.serializers import WelcomeMessageChatroomActivitySerializer
-        from apps.notification.models import NotificationType, OpenNotification
+    # @list_route(methods=['get', 'post'])
+    # def team_dashboard(self, request):
+    #     from forms import AutoMessageForm
+    #     from apps.chatroom.models import ChatroomActivity, ChatroomActivityType, ChatroomMember, Message, ChatroomActivityTypeManager
+    #     from apps.chatroom.serializers import WelcomeMessageChatroomActivitySerializer
+    #     from apps.notification.models import NotificationType, OpenNotification
 
-        team_user = User.objects.get(email='team@seshtutoring.com')
+    #     team_user = User.objects.get(email='team@seshtutoring.com')
 
-        if request.method == 'POST':
-            form = AutoMessageForm(request.POST)
-            if form.is_valid():
-                data = form.cleaned_data
-                text = data['message_text']
-                users = form.get_user_list()
+    #     if request.method == 'POST':
+    #         form = AutoMessageForm(request.POST)
+    #         if form.is_valid():
+    #             data = form.cleaned_data
+    #             text = data['message_text']
+    #             users = form.get_user_list()
 
-                for user_id in users:
-                    user = User.objects.get(pk=int(user_id))
+    #             for user_id in users:
+    #                 user = User.objects.get(pk=int(user_id))
 
-                    name = "The Sesh Team"
-                    desc = "We're so happy you're here! Any questions?"
-                    user_member = ChatroomMember.objects.get(user=user, chatroom__name=name, chatroom__description=desc)
-                    chatroom = user_member.chatroom
-                    team_member = ChatroomMember.objects.get(user=team_user, chatroom=chatroom)
+    #                 name = "The Sesh Team"
+    #                 desc = "We're so happy you're here! Any questions?"
+    #                 user_member = ChatroomMember.objects.get(user=user, chatroom__name=name, chatroom__description=desc)
+    #                 chatroom = user_member.chatroom
+    #                 team_member = ChatroomMember.objects.get(user=team_user, chatroom=chatroom)
 
-                    message = Message.objects.create(message=text, chatroom=chatroom, chatroom_member=team_member)
-                    activity_type = ChatroomActivityType.objects.get_activity_type(ChatroomActivityTypeManager.MESSAGE)
-                    activity = ChatroomActivity.objects.create(chatroom=chatroom, chatroom_activity_type=activity_type, activity_id=message.pk)
+    #                 message = Message.objects.create(message=text, chatroom=chatroom, chatroom_member=team_member)
+    #                 activity_type = ChatroomActivityType.objects.get_activity_type(ChatroomActivityTypeManager.MESSAGE)
+    #                 activity = ChatroomActivity.objects.create(chatroom=chatroom, chatroom_activity_type=activity_type, activity_id=message.pk)
 
-                    merge_vars = {
-                        "NAME": team_user.full_name,
-                        "MESSAGE": text
-                    }
-                    data = {
-                        "chatroom_activity": WelcomeMessageChatroomActivitySerializer(activity).data,
-                    }
-                    notification_type = NotificationType.objects.get(identifier="NEW_MESSAGE")
-                    OpenNotification.objects.create(user, notification_type, data, merge_vars, None)
+    #                 merge_vars = {
+    #                     "NAME": team_user.full_name,
+    #                     "MESSAGE": text
+    #                 }
+    #                 data = {
+    #                     "chatroom_activity": WelcomeMessageChatroomActivitySerializer(activity).data,
+    #                 }
+    #                 notification_type = NotificationType.objects.get(identifier="NEW_MESSAGE")
+    #                 OpenNotification.objects.create(user, notification_type, data, merge_vars, None)
 
-                return Response(form.get_user_list())
+    #             return Response(form.get_user_list())
 
-        else:
-            form = AutoMessageForm()
+    #     else:
+    #         form = AutoMessageForm()
 
-        return render(request, 'team_dashboard.html', {'team_user': team_user, 'form': form})
+    #     return render(request, 'team_dashboard.html', {'team_user': team_user, 'form': form})
 
     @list_route(methods=['post'])
     def populate_welcome_messages(self, request):
@@ -150,7 +152,7 @@ class UserViewSet(viewsets.ModelViewSet):
         desc = "We're so happy you're here! Any questions?"
         text_2 = "Welcome! Please reach out to us here if you ever want to chat. Problems, questions, suggestions, whatever it may be, don't be shy. We're here to help out however we can."
 
-        for user in User.objects.all():
+        for user in User.objects.all().exlude():
             text_1 = "Hey {}!".format(user.first_name)
             chatroom = Chatroom.objects.create(name=name, description=desc)
             conversation = Conversation.objects.create(chatroom=chatroom)
@@ -309,6 +311,8 @@ class UserViewSet(viewsets.ModelViewSet):
                 }
                 notification_type = NotificationType.objects.get(identifier="NEW_MESSAGE")
                 OpenNotification.objects.create(user, notification_type, data, merge_vars, None)
+                # small delay to differentiate timestamps
+                time.sleep(1)
                 text = "Welcome! Please reach out to us here if you ever want to chat. Problems, questions, suggestions, whatever it may be, don't be shy. We're here to help out however we can."
                 message = Message.objects.create(message=text, chatroom=chatroom, chatroom_member=team_member)
                 activity_type = ChatroomActivityType.objects.get_activity_type(ChatroomActivityTypeManager.MESSAGE)
