289e303a830c52db395d9cc40a37f94c5f640989
Zach Saraf <zach@seshtutoring.com>
2016-02-13 02:08:13 +0000

    add url for individual messaging tool

--- apps/tools/urls.py
+++ apps/tools/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from apps.tools import views
 
+
 urlpatterns = [
     url(r'leaderboard', views.Leaderboard.as_view(), name='leaderboard'),
     url(r'dashboard', views.Dashboard.as_view(), name='dashboard'),
