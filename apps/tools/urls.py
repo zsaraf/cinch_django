4d4ee267820d644e3e064a6b599473213253a18b
Lilli Oetting <loetting@stanford.edu>
2016-02-16 15:06:37 -0800

    merge course group tool

--- apps/tools/urls.py
+++ apps/tools/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'leaderboard', views.Leaderboard.as_view(), name='leaderboard'),
     url(r'dashboard', views.Dashboard.as_view(), name='dashboard'),
     url(r'chat', views.TeamChat.as_view(), name='chat'),
-    url(r'course_groups', views.CourseGroupDash.as_view(), name='course_groups')
+    url(r'course_groups', views.CourseGroupDash.as_view(), name='course_groups'),
+    url(r'merge', views.MergeCourseGroups.as_view(), name='merge')
 ]
--- apps/tools/views.py
+++ apps/tools/views.py
@@ -9,10 +9,13 @@ class CourseGroupDash(TemplateView):
     template_name = 'course_group_dashboard.html'
 
     def get_context_data(self, **kwargs):
-        from apps.group.models import CourseGroup, CourseGroupMember
-        from apps.chatroom.models import Message, Upload
+        from forms import FilterForm
+        from apps.group.models import CourseGroup
 
         context = super(CourseGroupDash, self).get_context_data(**kwargs)
+
+        context['filter_form'] = self.kwargs.get('filter_form', FilterForm())
+        context['course_groups'] = self.kwargs.get('course_groups', None)
 
         course_group_count = CourseGroup.objects.all().count()
         course_groups_lonely = CourseGroup.objects.raw("SELECT * FROM(SELECT cg.id, cg.timestamp, cg.course_id, cg.professor_name, COUNT(*) as count FROM course_group_member cm INNER JOIN course_group cg ON cm.course_group_id=cg.id INNER JOIN students s ON cm.student_id = s.id INNER JOIN users u ON s.user_id=u.id WHERE u.is_test=0 GROUP BY cm.course_group_id) as temp WHERE count=1")
@@ -20,20 +23,37 @@ class CourseGroupDash(TemplateView):
 
         context['num_groups'] = course_group_count
         context['num_lonely_groups'] = lonely_count
-        course_groups = CourseGroup.objects.all()
-        context_course_groups = []
-        for group in course_groups:
-            course_group = {}
-            course_group['num_uploads'] = Upload.objects.filter(chatroom=group.chatroom).count()
-            course_group['num_messages'] = Message.objects.filter(chatroom=group.chatroom).count()
-            course_group['num_students'] = CourseGroupMember.objects.filter(course_group=group).count()
-            course_group['course_name'] = group.course.get_readable_name()
-            course_group['professor_name'] = group.professor_name
-            course_group['school_name'] = group.course.school.name
-            course_group['timestamp'] = group.timestamp
-            context_course_groups.append(course_group)
-        context['course_groups'] = context_course_groups
+
         return context
+
+    def post(self, request, *args, **kwargs):
+        from apps.group.models import CourseGroup, CourseGroupMember
+        from apps.chatroom.models import Message, Upload
+        from forms import FilterForm
+
+        if 'update_filter' in request.POST:
+
+            form = FilterForm(request.POST)
+            self.kwargs['filter_form'] = form
+            if form.is_valid():
+                data = form.cleaned_data
+                filter_type = data['filter_type']
+
+                course_groups = CourseGroup.objects.all()
+                context_course_groups = []
+                for group in course_groups:
+                    course_group = {}
+                    course_group['num_uploads'] = Upload.objects.filter(chatroom=group.chatroom).count()
+                    course_group['num_messages'] = Message.objects.filter(chatroom=group.chatroom).count()
+                    course_group['num_students'] = CourseGroupMember.objects.filter(course_group=group).count()
+                    course_group['course_name'] = group.course.get_readable_name()
+                    course_group['professor_name'] = group.professor_name
+                    course_group['school_name'] = group.course.school.name
+                    course_group['timestamp'] = group.timestamp
+                    context_course_groups.append(course_group)
+                self.kwargs['course_groups'] = context_course_groups
+
+                return super(CourseGroupDash, self).get(request, args, **kwargs)
 
 
 class Leaderboard(TemplateView):
@@ -157,6 +177,68 @@ class TeamChat(TemplateView):
                 self.kwargs['message_form'] = message_form
 
             return super(TeamChat, self).get(request, args, **kwargs)
+
+class MergeCourseGroups(TemplateView):
+    template_name = 'merge_course_groups.html'
+
+    def get_context_data(self, **kwargs):
+        from apps.account.models import User
+        from apps.group.forms import MergeGroupsForm
+
+        context = super(MergeCourseGroups, self).get_context_data(**kwargs)
+        team_user = User.objects.get(email='team@seshtutoring.com')
+        form = MergeGroupsForm()
+        context = {'team_user': team_user, 'form': form}
+        return context
+
+    def post(self, request, *args, **kwargs):
+        from apps.group.forms import MergeGroupsForm
+        from apps.notification.models import NotificationType, OpenNotification
+        from apps.group.models import CourseGroup, CourseGroupMember
+        from apps.chatroom.models import ChatroomMember
+
+        form = MergeGroupsForm(request.POST)
+        if form.is_valid():
+            groups = form.get_groups()
+
+            first_group = CourseGroup.objects.get(pk=groups[0])
+            second_group = CourseGroup.objects.get(pk=groups[1])
+
+            if first_group.is_past or second_group.is_past:
+                return HttpResponseRedirect('/django/tools/merge/')
+
+            winner_group = first_group
+            loser_group = second_group
+
+            if first_group.coursegroupmember_set.all().count() < second_group.coursegroupmember_set.all().count():
+                winner_group = second_group
+                loser_group = first_group
+
+            loser_group.is_past = True
+            loser_group.save()
+
+            loser_members = CourseGroupMember.objects.filter(course_group=loser_group, is_past=False)
+            for member in loser_members:
+                member.is_past = True
+                member.save()
+
+                try:
+                    # see if they're already a member of the other group
+                    existing_member = CourseGroupMember.objects.get(course_group=winner_group, student=member.student)
+                    if existing_member.is_past:
+                        existing_member.is_past = False
+                        existing_member.save()
+                except CourseGroupMember.DoesNotExist:
+                    # they weren't already a member, create a new one
+                    CourseGroupMember.objects.create(course_group=winner_group, student=member.student, is_past=False)
+                    ChatroomMember.objects.create(chatroom=winner_group.chatroom, user=member.student.user)
+
+                # send refresh user notification
+                notification_type = NotificationType.objects.get(identifier='REFRESH_FULL_USER_INFORMATION')
+                OpenNotification.objects.create(member.student.user, notification_type, None, None, None)
+
+        return HttpResponseRedirect('/django/tools/merge/')
+
 
 
 class Dashboard(TemplateView):
