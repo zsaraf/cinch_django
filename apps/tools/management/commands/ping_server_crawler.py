82ceffcc5ff4bb6493221304d0e519624cdc61f0
Zach Saraf <zsaraf@stanford.edu>
2016-02-17 14:45:05 -0800

    Implementing manage.py command

--- apps/tools/management/commands/ping_server_crawler.py
+++ apps/tools/management/commands/ping_server_crawler.py
@@ -1,6 +1,12 @@
+from django.core.management.base import BaseCommand, CommandError
 from urllib import urlopen
 import time
 from sesh import slack_utils
+
+class Command(BaseCommand):
+    help = 'Pings sesh server'
+
+    def add_arguments(self, parser):
 
 while True:
     try:
